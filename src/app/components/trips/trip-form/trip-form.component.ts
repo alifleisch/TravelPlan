fb6c9229706c4a17c597bd38681ab9814671d18f
Alina Fleiscmann <alina.fleischmannn@gmail.com>
2024-08-07 18:23:38 +0200

    trip form styles

--- src/app/components/trips/trip-form/trip-form.component.ts
+++ src/app/components/trips/trip-form/trip-form.component.ts
@@ -1,13 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-trips-form',
   standalone: true,
   templateUrl: './trip-form.component.html',
   styleUrls: ['./trip-form.component.scss'],
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule,
+    MatButtonModule, MatInputModule,
+    MatFormFieldModule, FormsModule]
 })
 export class TripFormComponent {
   @Output() tripCreated = new EventEmitter<any>();
@@ -21,7 +26,15 @@ export class TripFormComponent {
   submitForm() {
     if (this.newTrip.name && this.newTrip.location && this.newTrip.date) {
       this.tripCreated.emit(this.newTrip);
-      this.newTrip = { name: '', location: '', date: '' };
+      this.resetForm();
     }
+  }
+
+  resetForm() {
+    this.newTrip = {
+      name: '',
+      location: '',
+      date: ''
+    };
   }
 }
